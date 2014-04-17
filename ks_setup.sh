d93862d2936ed1c48cea31a18f34ee3ae33ed4ab
thekingh <ksingh.pa@gmail.com>
2014-04-17 19:29:44 -0400

    Added description, getting acquainted with git

--- ks_setup.sh
+++ ks_setup.sh
@@ -1,3 +1,8 @@
+#Author: Kabir Singh
+#Date:   17 April 2014
+#
+#Description: A quick script to setup a new host with some
+#             of the settings I've come to enjoy.
 #!/bin/sh
 
 cd ~
@@ -11,6 +16,7 @@ echo export TERM='xterm-256color' >> .bashrc
 
 echo Copying over .vimrc...
 git clone git://github.com/thekingh/etc/.vimrc
+
 
 
 
