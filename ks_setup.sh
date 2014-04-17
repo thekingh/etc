820a0fee3fe590279a180e67dd4b44ec4f87018e
thekingh <thekingh@crunchbang-vm>
2014-04-17 19:22:49 -0400

    First commit

---
+++ ks_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd ~
+
+echo Installing Vim...
+sudo apt-get install vim-gnome
+
+echo Enabling 256 Color support...
+sudo apt-get ncruses-term
+echo export TERM='xterm-256color' >> .bashrc
+
+echo Copying over .vimrc...
+
+
+
+
