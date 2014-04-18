c3e4fd3522d42ad2b97c125779b0836946ed99f5
thekingh <ksingh.pa@gmail.com>
2014-04-18 19:51:42 -0400

    Added plugin and colorscheme addition

--- ks_setup.sh
+++ ks_setup.sh
@@ -3,19 +3,44 @@
 #
 #Description: A quick script to setup a new host with some
 #             of the settings I've come to enjoy.
+#Hmm? VV
+#. /usr/sup/use/use.sh
+
 #!/bin/sh
 
 cd ~
-
-echo Installing Vim...
-sudo apt-get install vim-gnome
 
 echo Enabling 256 Color support...
 sudo apt-get ncruses-term
 echo export TERM='xterm-256color' >> .bashrc
 
+echo Installing Vim...
+sudo apt-get install vim-gnome
+
 echo Copying over .vimrc...
-git clone git://github.com/thekingh/etc/.vimrc
+git clone git://github.com/thekingh/etc/.vimrc /etc
+mv etc/.vimrc .vimrc
+
+echo Installing pathogen...
+mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+curl -Sso ~/.vim/autoload/pathogen.vim \
+     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+echo Installing some plugins...
+git clone https://github.com/vim-scripts/Conque-Shell.git ~/.vim/bundle/conque/
+git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp/
+
+echo Installing colorschemes...
+mkdir ~/.vim/colors 
+git clone https://github.com/w0ng/vim-hybrid.git ~/.vim/colors/hybrid
+mv ~/.vim/colors/hybrid/colors/*.vim ../..
+git clone https://github.com/croaker/mustang-vim.git ~/.vim/colors/mustang
+mv ~/.vim/colors/mustang/colors/*.vim ../..
+git clone https://github.com/tomasr/molokai.git ~/.vim/colors/molokai
+mv ~/.vim/colors/molokai/colors/*.vim ../..
+
+
+
 
 
 
